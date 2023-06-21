7c85afe2aed75e9564d8fe7fca7d2abfd34b7b5f
babu <gaddharaju2104@gmail>
2023-06-21 18:15:25 +0530

    chngd

--- Code.tf
+++ Code.tf
@@ -1,80 +1,213 @@
-# Configure the Azure provider
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.51.0"
+    }
+  }
+}
+
+terraform { 
+
+  backend "azurerm" { 
+
+  } 
+} 
+
+
+
 provider "azurerm" {
-  features {}
+  features {
+
+  }
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "app_rg" {
-  name     = "myapp-resource-group"      # Specify the name of the resource group
-  location = "West US"                   # Set the location for the resource group
+resource "azurerm_resource_group" "RG1" {
+  name     = var.RgName
+  location = var.RGlocation
+
+tags = {
+ project ="learning"
+ env = "dev"
+
 }
 
-# Create a virtual network
-resource "azurerm_virtual_network" "app_vnet" {
-  name                = "myapp-vnet"                 # Specify the name of the virtual network
-  address_space       = ["10.0.0.0/16"]              # Define the IP address range for the VNet
-  location            = azurerm_resource_group.app_rg.location
-  resource_group_name = azurerm_resource_group.app_rg.name
 }
 
-# Create a subnet within the virtual network
-resource "azurerm_subnet" "app_subnet" {
-  name                 = "myapp-subnet"              # Specify the name of the subnet
-  resource_group_name  = azurerm_resource_group.app_rg.name
-  virtual_network_name = azurerm_virtual_network.app_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]             # Define the IP address range for the subnet
+resource "azurerm_virtual_network" "myvnet" {
+  name                = var.vnetName
+  address_space       = var.vnetAddress
+  location            = azurerm_resource_group.RG1.location
+  resource_group_name = azurerm_resource_group.RG1.name
+
+tags = {
+ project ="learning"
+ env = "dev"
+
 }
 
-# Create a network interface and associate it with the subnet
-resource "azurerm_network_interface" "app_nic" {
-  name                = "myapp-nic"                  # Specify the name of the network interface
-  location            = azurerm_resource_group.app_rg.location
-  resource_group_name = azurerm_resource_group.app_rg.name
+}
+
+resource "azurerm_subnet" "mysnet" {
+  count                = length(var.subnet_address_prefixes)
+  name                 = element(var.subnetnames, count.index)
+  resource_group_name  = azurerm_resource_group.RG1.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = [var.subnet_address_prefixes[count.index]]
+  service_endpoints    = ["Microsoft.Storage"]
+
+
+
+}
+resource "azurerm_network_interface" "nics" {
+  count               = length(var.privateaddresses)
+  name                = element(var.nicnames, count.index)
+  resource_group_name = azurerm_resource_group.RG1.name
+  location            = azurerm_resource_group.RG1.location
+  ip_configuration {
+    name                          = element(var.ipconfignames, count.index)
+    subnet_id                     = azurerm_subnet.mysnet[count.index].id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.privateaddresses[count.index]
+  }
+tags = {
+ project ="learning"
+ env = "dev"
+}
+
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = var.pubip
+  location            = azurerm_resource_group.RG1.location
+  resource_group_name = azurerm_resource_group.RG1.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+tags = {
+ project ="learning"
+ env = "dev"
+}
+
+}
+
+resource "azurerm_bastion_host" "bhost" {
+  name                = var.basthost
+  location            = azurerm_resource_group.RG1.location
+  resource_group_name = azurerm_resource_group.RG1.name
 
   ip_configuration {
-    name                          = "myapp-ipconfig"  # Specify the name of the IP configuration
-    subnet_id                     = azurerm_subnet.app_subnet.id
-    private_ip_address_allocation = "Dynamic"        # Allocate IP address dynamically
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.mysnet[2].id
+    public_ip_address_id = azurerm_public_ip.pip.id
   }
+tags = {
+ project ="learning"
+ env = "dev"
+
 }
 
-# Create a virtual machine and associate it with the network interface
-resource "azurerm_virtual_machine" "app_vm" {
-  name                  = "myapp-vm"                   # Specify the name of the virtual machine
-  location              = azurerm_resource_group.app_rg.location
-  resource_group_name   = azurerm_resource_group.app_rg.name
-  network_interface_ids = [azurerm_network_interface.app_nic.id]
-  vm_size               = "Standard_DS1_v2"            # Specify the size of the virtual machine
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "myapp-osdisk"                 # Specify the name of the OS disk
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "myapp-vm"                        # Specify the computer name of the VM
-    admin_username = "adminuser"                       # Set the admin username for the VM
-    admin_password = "Password1234!"                   # Set the admin password for the VM
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello World' > /home/adminuser/index.html",
-      "sudo mv /home/adminuser/index.html /var/www/html/"
-    ]
-  }
 }
 
-# Output the public IP address for accessing the deployed application
-output "public_ip_address" {
-  value = azurerm_network_interface.app_nic.private_ip_address
+resource "random_password" "password" {
+  length           = 8
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "azurerm_linux_virtual_machine" "vms" {
+  count                           = length(var.vmnames)
+  name                            = element(var.vmnames, count.index)
+  resource_group_name             = azurerm_resource_group.RG1.name
+  location                        = azurerm_resource_group.RG1.location
+  size                            = "Standard_B2s"
+  admin_username                  = element(var.vmunames, count.index)
+  admin_password                  = azurerm_key_vault_secret.passwordsecret.value
+  disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.nics[count.index].id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+
+  source_image_reference {
+    publisher = element(var.vm_os, count.index) == "UbuntuServer" ? var.ubuntu_publisher : var.centos_publisher
+    offer     = element(var.vm_os, count.index) == "UbuntuServer" ? var.ubuntu_offer : var.centos_offer
+    sku       = element(var.vm_os, count.index) == "UbuntuServer" ? var.ubuntu_sku : var.centos_sku
+    version   = element(var.vm_os, count.index) == "UbuntuServer" ? "latest" : "latest"
+  }
+tags = {
+ project ="learning"
+ env = "dev"
+
+}
+
+}
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storage
+  resource_group_name      = azurerm_resource_group.RG1.name
+  location                 = azurerm_resource_group.RG1.location
+  account_tier             = "Standard"
+  account_replication_type = var.replitype
+  network_rules {
+    default_action             = "Deny"
+    virtual_network_subnet_ids = azurerm_subnet.mysnet[*].id
+
+  }
+
+tags = {
+ project ="learning"
+ env = "dev"
+
+}
+
+}
+
+
+resource "azurerm_key_vault" "myvault" {
+  name                = var.keyvaults
+  resource_group_name = azurerm_resource_group.RG1.name
+  location            = azurerm_resource_group.RG1.location
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  enabled_for_disk_encryption = true
+
+  purge_protection_enabled = false
+ 
+tags = {
+ project ="learning"
+ env = "dev"
+}
+
+}
+
+locals {
+objectIds = {
+currentid = data.azurerm_client_config.current.object_id
+myid= var.myObjectId
+adoid= var.ADO_Service_Account_ObjectId
+}
+}
+
+resource "azurerm_key_vault_access_policy" "koushikPolicy" {
+for_each= local.objectIds
+key_vault_id= azurerm_key_vault.myvault.id
+tenant_id= data.azurerm_client_config.current.tenant_id
+object_id= each.value
+secret_permissions = ["Get", "Set", "List", "Delete", "Recover", "Restore", "Set", "Purge"]
+depends_on=[azurerm_key_vault.myvault]
+
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault_secret" "passwordsecret" {
+  name         = var.secretkeys
+  value        = random_password.password.result 
+  key_vault_id = azurerm_key_vault.myvault.id
+depends_on=[azurerm_key_vault_access_policy.koushikPolicy]
 }
