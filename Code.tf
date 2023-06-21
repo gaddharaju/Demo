b418bc623758940697d3996b121a9cad54c6c8ea
babu <gaddharaju2104@gmail>
2023-06-21 14:38:58 +0530

    Latest

--- Code.tf
+++ Code.tf
@@ -3,70 +3,109 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group to contain the resources
+# Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"      # Specify the name of the resource group
-  location = "West US"                     # Set the location for the resource group
+  name     = "example-resource-group"
+  location = "West Europe"
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"                     # Specify the name of the virtual network
-  address_space       = ["10.0.0.0/16"]                    # Define the IP address range for the VNet
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
-# Create a subnet within the virtual network
+# Create a subnet
 resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"                  # Specify the name of the subnet
+  name                 = "example-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]                   # Define the IP address range for the subnet
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Create a network interface within the resource group and virtual network
+# Create a public IP address
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+# Create a network security group
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Allow inbound HTTP traffic on port 80
+resource "azurerm_network_security_rule" "http" {
+  name                        = "allow-http"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+# Create a network interface
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic"                      # Specify the name of the network interface
+  name                = "example-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "example-ipconfig"      # Specify the name of the IP configuration
+    name                          = "example-ipconfig"
     subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"              # Allocate IP address dynamically
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
-# Create a virtual machine within the resource group and attach the network interface
+# Create a virtual machine
 resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"                      # Specify the name of the virtual machine
+  name                  = "example-vm"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
+  vm_size               = "Standard_DS2_v2"
   network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"                 # Specify the size of the virtual machine
 
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
   storage_os_disk {
-    name              = "example-osdisk"                    # Specify the name of the OS disk
+    name              = "example-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "examplevm"                            # Specify the computer name of the VM
-    admin_username = "adminuser"                            # Set the admin username for the VM
-    admin_password = "Password1234!"                        # Set the admin password for the VM
+    computer_name  = "example-vm"
+    admin_username = "adminuser"
+    admin_password = "AdminPassword1234!"
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install apache2 -y",
+      "sudo sh -c 'echo \"Hello, World!\" > /var/www/html/index.html'"
+    ]
   }
 }
