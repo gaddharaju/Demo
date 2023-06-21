dcaad99706f7ff23f99631d3ec2470b5995c79be
babu <gaddharaju2104@gmail>
2023-06-21 15:36:11 +0530

    Updated

--- Code.tf
+++ Code.tf
@@ -1,78 +1,59 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.51.0"
-    }
-  }
-}
-
-terraform {
-
-  backend "azurerm" {
-  } 
-} 
-
 # Configure the Azure provider
 provider "azurerm" {
   features {}
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example1" {
-  name     = "resource-group"      # Specify the name of the resource group
-  location = "West US"                     # Set the location for the resource group
+resource "azurerm_resource_group" "app_rg" {
+  name     = "myapp-resource-group"      # Specify the name of the resource group
+  location = "West US"                   # Set the location for the resource group
 }
 
 # Create a virtual network
-resource "azurerm_virtual_network" "example2" {
-  name                = "example-vnet"                     # Specify the name of the virtual network
-  address_space       = ["10.0.0.0/16"]                    # Define the IP address range for the VNet
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_virtual_network" "app_vnet" {
+  name                = "myapp-vnet"                     # Specify the name of the virtual network
+  address_space       = ["10.0.0.0/16"]                  # Define the IP address range for the VNet
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
 }
 
 # Create a subnet within the virtual network
-resource "azurerm_subnet" "example3" {
-  name                 = "example-subnet"                  # Specify the name of the subnet
-  resource_group_name  = azurerm_resource_group.example1.name
-  virtual_network_name = azurerm_virtual_network.example2.name
-  address_prefixes     = ["10.0.1.0/24"]                   # Define the IP address range for the subnet
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "myapp-subnet"                  # Specify the name of the subnet
+  resource_group_name  = azurerm_resource_group.app_rg.name
+  virtual_network_name = azurerm_virtual_network.app_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]                 # Define the IP address range for the subnet
 }
 
 # Create a public IP address
-resource "azurerm_public_ip" "example3" {
-  name                = "example-public-ip"                # Specify the name of the public IP
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example1.name
-  allocation_method   = "Static"                           # Choose between "Static" or "Dynamic" allocation
-
-  tags = {
-    environment = "dev"
-  }
+resource "azurerm_public_ip" "app_public_ip" {
+  name                = "myapp-public-ip"                # Specify the name of the public IP
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+  allocation_method   = "Static"                         # Choose between "Static" or "Dynamic" allocation
 }
 
 # Create a network interface and associate it with the subnet and public IP
-resource "azurerm_network_interface" "example4" {
-  name                = "example-nic"                      # Specify the name of the network interface
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example1.name
+resource "azurerm_network_interface" "app_nic" {
+  name                = "myapp-nic"                      # Specify the name of the network interface
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
 
   ip_configuration {
-    name                          = "example-ipconfig"      # Specify the name of the IP configuration
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"              # Allocate IP address dynamically
-    public_ip_address_id          = azurerm_public_ip.example.id
+    name                          = "myapp-ipconfig"      # Specify the name of the IP configuration
+    subnet_id                     = azurerm_subnet.app_subnet.id
+    private_ip_address_allocation = "Dynamic"            # Allocate IP address dynamically
+    public_ip_address_id          = azurerm_public_ip.app_public_ip.id
   }
 }
 
 # Create a virtual machine and associate it with the network interface
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"                      # Specify the name of the virtual machine
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"                 # Specify the size of the virtual machine
+resource "azurerm_virtual_machine" "app_vm" {
+  name                  = "myapp-vm"                      # Specify the name of the virtual machine
+  location              = azurerm_resource_group.app_rg.location
+  resource_group_name   = azurerm_resource_group.app_rg.name
+  network_interface_ids = [azurerm_network_interface.app_nic.id]
+  vm_size               = "Standard_DS1_v2"               # Specify the size of the virtual machine
 
   storage_image_reference {
     publisher = "Canonical"
@@ -82,24 +63,32 @@ resource "azurerm_virtual_machine" "example" {
   }
 
   storage_os_disk {
-    name              = "example-osdisk"                    # Specify the name of the OS disk
+    name              = "myapp-osdisk"                    # Specify the name of the OS disk
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "examplevm"                            # Specify the computer name of the VM
-    admin_username = "adminuser"                            # Set the admin username for the VM
-    admin_password = "Password1234!"                        # Set the admin password for the VM
+    computer_name  = "myapp-vm"                           # Specify the computer name of the VM
+    admin_username = "adminuser"                          # Set the admin username for the VM
+    admin_password = "Password1234!"                      # Set the admin password for the VM
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y apache2",
+      "echo 'Hello World' | sudo tee /var/www/html/index.html"
+    ]
+  }
 }
 
-# Output the public IP address for accessing the VM
+# Output the public IP address for accessing the deployed application
 output "public_ip_address" {
-  value = azurerm_public_ip.example.ip_address
+  value = azurerm_public_ip.app_public_ip.ip_address
 }
